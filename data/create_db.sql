0d9b777c32928b95ef5c825feb7d408ee87a2b56
AdrienDurup <adrien.durup@gmail.com>
2021-11-12 10:34:52 +0100

    correction table

--- data/create_db.sql
+++ data/create_db.sql
@@ -8,29 +8,29 @@ DROP TABLE IF EXISTS "list";
 CREATE TABLE "list" (
     "id" SERIAL PRIMARY KEY,
     "name" VARCHAR(255) NOT NULL,
-    "created_at" TIMESTAMP NOT NULL DEFAULT NOW(),
-    "updated_at" TIMESTAMP NOT NULL DEFAULT NOW()
+    "created_at" TIMESTAMPTZ NOT NULL DEFAULT NOW(),
+    "updated_at" TIMESTAMPTZ
 );
 CREATE TABLE "card" (
     "id" SERIAL PRIMARY KEY,
     "title" VARCHAR(255),
-    "color" VARCHAR(255),
+    "color" VARCHAR(255) NOT NULL DEFAULT '#FFF',
     "position" INTEGER,
-    "list_id" INT NOT NULL REFERENCES "list"("id"),
-    "created_at" TIMESTAMP NOT NULL DEFAULT NOW(),
-    "updated_at" TIMESTAMP NOT NULL DEFAULT NOW()
+    "list_id" INT NOT NULL REFERENCES "list"("id") ON DELETE CASCADE,
+    "created_at" TIMESTAMPTZ NOT NULL DEFAULT NOW(),
+    "updated_at" TIMESTAMPTZ
 );
 CREATE TABLE "label" (
     "id" SERIAL PRIMARY KEY,
     "name" VARCHAR(255) NOT NULL,
-    "color" VARCHAR(255),
-    "created_at" TIMESTAMP NOT NULL DEFAULT NOW(),
-    "updated_at" TIMESTAMP NOT NULL DEFAULT NOW()
+    "color" VARCHAR(255) NOT NULL DEFAULT '#FFF',
+    "created_at" TIMESTAMPTZ NOT NULL DEFAULT NOW(),
+    "updated_at" TIMESTAMPTZ
 );
 CREATE TABLE "card_has_label" (
-    "card_id" INT NOT NULL REFERENCES "card"("id"),
-    "label_id" INT NOT NULL REFERENCES "label"("id"),
-    "created_at" TIMESTAMP NOT NULL DEFAULT NOW(),
-    "updated_at" TIMESTAMP NOT NULL DEFAULT NOW()
+    "card_id" INT NOT NULL REFERENCES "card"("id") ON DELETE CASCADE,
+    "label_id" INT NOT NULL REFERENCES "label"("id") ON DELETE CASCADE,
+    "created_at" TIMESTAMPTZ NOT NULL DEFAULT NOW(),
+    "updated_at" TIMESTAMPTZ
 );
 COMMIT;
