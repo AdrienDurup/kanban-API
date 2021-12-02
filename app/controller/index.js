63de45aba320675702724fcec057a54877407a03
AdrienDurup <adrien.durup@gmail.com>
2021-12-02 01:03:51 +0100

    cleaning controllers

--- app/controller/index.js
+++ app/controller/index.js
@@ -1,15 +1,7 @@
-const { listController } = require("./listController");
-const { cardController } = require("./cardController");
-const { labelController } = require("./labelController");
 const { restController } = require("./restController");
-const { cardHasLabelController } = require("./cardHasLabelController");
 const { options } = require("./restController.options.js");
 
 module.exports = {
-    listController,
-    cardController,
-    labelController,
-    cardHasLabelController,
     restController,
     options
 };
